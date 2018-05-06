8a8c85077ca7df937b76b67d383725c8cf4f4ce4
ChromaBey <36361913+ChromaBey@users.noreply.github.com>
2018-05-06 20:02:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("TutoBot, !help");
-    console.log("Le bot a bien ete connecte");
+    bot.user.setGame("ChromaBOT : /help");
+    console.log("Bot Online!");
 });
 
 bot.login(NDQyNzI1MjI3MDcyNDU0NjU3.DdDFKg.xIJISjE_jlpb5pmYnDIuYBz-Tzw);
